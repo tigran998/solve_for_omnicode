b76858c2a522d339a5d5eadb676e0c4ca54e2ca6
tumanyantigran1998 <tigrantumanyan1998@gmail.com>
2023-11-28 20:36:40 +0400

    project first commit

---
+++ src/Body.js
@@ -0,0 +1,17 @@
+import './Body.css'
+
+function Body() {
+    return (
+        <div className="body">
+            <div className='top-left'>
+                
+            </div>
+            <div className='top-right'>
+                
+            </div>
+
+        </div>
+
+    )
+}
+export default Body
---
+++ src/Header.js
@@ -0,0 +1,18 @@
+
+
+function Header() {
+    return (
+        <div className="header">
+
+            <div className='headerContent'>Experts.ai</div>
+
+            <div>
+                <div className='headerContent1'>How it works</div>
+                <div className='headerContent1'>Abous us</div>
+                <div className='headerContent1'>Services</div>
+            </div>
+
+        </div>
+    )
+}
+export default Header
